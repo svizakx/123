2255965b3d2a2e3686c6e6219bd1ad9a901aea86
kryha5555 <kryha5555@gmail.com>
2020-11-01 01:08:23 +0100

    get

--- src/Pages/TestPage.js
+++ src/Pages/TestPage.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import APIService from '../Services/APIService';
 
 export default class TestPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            attributes: []
+        };
+    }
+
+    componentDidMount() {
+        APIService.get('attributes')
+            .then(result => {
+                const attributes = result;
+                this.setState({ attributes });
+            })
+
+    }
+
     render() {
         return (
-            <h1>Test</h1>
+            <div>
+                <h1>Test</h1>
+                <ul>
+                    {this.state.attributes.map(att => <li>{att.id}: {att.name}</li>)}
+                </ul>
+            </div>
         )
     }
 };
