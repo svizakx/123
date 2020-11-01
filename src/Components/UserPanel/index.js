053d235be0419bcbe8a76ab912836ac168821eb0
kryha5555 <kryha5555@gmail.com>
2020-11-01 22:02:09 +0100

    user or admin

---
+++ src/Components/UserPanel/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class UserPanel extends Component {
+    render() {
+        return (
+            <div>
+                Logged in as User.
+            </div>
+        );
+    }
+}
--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import UserPanel from "../../Components/UserPanel/index";
+import AdminPanel from "../../Components/AdminPanel/index";
+const roleToken = "role_token";
 
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ role: window.localStorage.getItem(roleToken) })
+  }
   render() {
-    return <h1>Home</h1>;
+    return (
+      <div>
+        <h1>Home</h1>
+        {this.state.role === "User" && <UserPanel />}
+        {this.state.role === "Admin" && <AdminPanel />}
+      </div>
+    );
   }
 }
