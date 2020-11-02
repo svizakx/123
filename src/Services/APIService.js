197dd15b534dffee18b2091380c7547a0e434cd4
Dominik Ruman <dr124@o2.pl>
2020-11-02 07:53:25 +0100

    Update APIService.js

--- src/Services/APIService.js
+++ src/Services/APIService.js
@@ -3,6 +3,7 @@ import Events from "./Events";
 import EventService from "./EventService";
 
 const authToken = "auth_token";
+const role = "role";
 
 export default class APIService {
   static API_URL = "https://api.rum.software";
@@ -49,6 +50,8 @@ export default class APIService {
   static _handle401(e) {
     if (e.response.status === 401) {
       EventService.Emit(Events.Unauthorized);
+      window.localStorage.removeItem(authToken);
+      window.localStorage.removeItem(role);
     } else throw e;
   }
 }
